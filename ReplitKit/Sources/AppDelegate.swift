f4b1d276088996430db194f0f4c5b656af851bd2
FERELKING242 <111065891+ferelking242@users.noreply.github.com>
2026-06-28 01:32:22 +0100

    feat: skip login, go direct to BrowserViewController

--- ReplitKit/Sources/AppDelegate.swift
+++ ReplitKit/Sources/AppDelegate.swift
@@ -2,15 +2,12 @@ import UIKit
 
   @main
   class AppDelegate: UIResponder, UIApplicationDelegate {
-
       var window: UIWindow?
-
       func application(_ application: UIApplication,
                        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
           window = UIWindow(frame: UIScreen.main.bounds)
-          let loginVC = LoginViewController()
-          let nav = UINavigationController(rootViewController: loginVC)
-          nav.setNavigationBarHidden(true, animated: false)
+          let nav = UINavigationController(rootViewController: BrowserViewController())
+          nav.setNavigationBarHidden(false, animated: false)
           window?.rootViewController = nav
           window?.makeKeyAndVisible()
           Logger.shared.log("App launched")
