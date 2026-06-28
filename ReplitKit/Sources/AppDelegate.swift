eb95100c3ab3483ca8f1049499ad3597528e54f5
FERELKING242 <111065891+ferelking242@users.noreply.github.com>
2026-06-28 01:19:36 +0100

    feat: start with native LoginViewController

--- ReplitKit/Sources/AppDelegate.swift
+++ ReplitKit/Sources/AppDelegate.swift
@@ -1,19 +1,20 @@
 import UIKit
 
-@main
-class AppDelegate: UIResponder, UIApplicationDelegate {
+  @main
+  class AppDelegate: UIResponder, UIApplicationDelegate {
 
-    var window: UIWindow?
+      var window: UIWindow?
 
-    func application(_ application: UIApplication,
-                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        window = UIWindow(frame: UIScreen.main.bounds)
-        let vc = BrowserViewController()
-        let nav = UINavigationController(rootViewController: vc)
-        nav.setNavigationBarHidden(false, animated: false)
-        window?.rootViewController = nav
-        window?.makeKeyAndVisible()
-        Logger.shared.log("App launched")
-        return true
-    }
-}
+      func application(_ application: UIApplication,
+                       didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+          window = UIWindow(frame: UIScreen.main.bounds)
+          let loginVC = LoginViewController()
+          let nav = UINavigationController(rootViewController: loginVC)
+          nav.setNavigationBarHidden(true, animated: false)
+          window?.rootViewController = nav
+          window?.makeKeyAndVisible()
+          Logger.shared.log("App launched")
+          return true
+      }
+  }
+  
